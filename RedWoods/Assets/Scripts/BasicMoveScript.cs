af3d9a216d1f4615a9cb59d40d71eec07635d392
rmstanton <rmstanton01@gmail.com>
2017-11-30 20:03:53 -0700

    committing ui stuff

---
+++ RedWoods/Assets/Scripts/BasicMoveScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMoveScript : MonoBehaviour {
+
+    Rigidbody2D rb;
+    bool inMenu = false;         // player movement is inactive when in menus
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update() { }
+
+
+    void FixedUpdate() { 
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        if (!inMenu)
+        {
+            Vector2 velocity = new Vector2(horizontalMove, verticalMove) * speed * Time.deltaTime;
+            rb.velocity = velocity;
+        }
+
+    }
+}
---
+++ RedWoods/Assets/Scripts/HouseUIScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseUIScript : MonoBehaviour {
+
+    
+    public GameObject shopCanvas;// =  GameObject.FindGameObjectWithTag("ShopCanvas");
+    GameObject player;
+    BasicMoveScript playerScript;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        //shopCanvas = GameObject.FindGameObjectWithTag("ShopCanvas");
+        shopCanvas.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Interact")) Debug.Log("Interact detected");
+
+	}
+
+    
+
+    void OnCollisionStay2D( Collision2D c ) {
+        
+        if ( c.gameObject.tag == "Player" &&  Input.GetButtonDown("Interact")) {
+            Debug.Log("Launcha da menu");
+            shopCanvas.SetActive(true);
+        }
+
+    }
+
+}
